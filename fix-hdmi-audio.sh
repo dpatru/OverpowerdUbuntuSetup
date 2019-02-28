871aa567494bcc5193e56feb0cd65cd23be8b7da
Daniel Patru <dpatru@gmail.com>
2019-02-28 09:39:16 -0500

    Update fix-hdmi-audio.sh

--- fix-hdmi-audio.sh
+++ fix-hdmi-audio.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Make this file executable (chmod +x fix-hdmi-audio.sh) 
 # and put this file in /usr/local/bin/
+# See https://askubuntu.com/a/1061125
 setpci -s 01:00.0 0x488.l=0x2000000:0x2000000
 rmmod nvidia-uvm nvidia-drm nvidia-modeset nvidia
 sh -c 'echo 1 > /sys/bus/pci/devices/0000:01:00.0/remove'
